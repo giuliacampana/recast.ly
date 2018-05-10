263793d24b3404806ab74d6cb57727c8374b0f41
giuliacampana <giuliaecampana@gmail.com>
2018-05-10 11:34:22 -0700

    Finished sprint

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,10 +13,16 @@ class Search extends React.Component {
     });
   }
 
+  handleKeyPress(event) {
+    console.log("Key press event detected event.key", event.key);
+    if (event.key === 'Enter') {
+      this.props.search.call(this, this.state.text);
+    }
+  }
   render() {
     return (
       <div className="search-bar form-inline">
-      <input className="form-control" type="text" onChange={(e)=> this.onSearch(e)} />
+      <input className="form-control" type="text" onChange={(e)=> this.onSearch(e)} onKeyPress={this.handleKeyPress.bind(this)} />
       <button className="btn hidden-sm-down" onClick={this.props.search.bind(this, this.state.text)}>
       <span className="glyphicon glyphicon-search"></span>
       </button>
